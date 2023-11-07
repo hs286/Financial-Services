c2fd9540a48afe44c1d511b70ca37bd9e3ef0fad
ali.hassan717 <ali.hassan@cooperativecomputing.com>
2023-11-07 11:37:01 +0500

    error handling better

--- server.js
+++ server.js
@@ -1,93 +1,3 @@
-// const express = require("express");
-// const puppeteer = require("puppeteer");
-// const cors = require("cors");
-
-// const app = express();
-// const port = 4000; // Change this to your desired port
-
-// app.use(cors());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// app.get('/', (req, res) => {
-//   res.send('Hello, World! This is your Node.js app for live.');
-// });
-
-// // Handle form submission
-// app.post("/financial-services", async (req, res) => {
-//   console.log(req.body);
-//   const {
-//     first_name,
-//     email,
-//     phone,
-//     zip_code,
-//     types_of_dept,
-//     other_dept_type,
-//     dept_owned,
-//   } = req.body;
-//   const browser = await puppeteer.launch({ headless: "new" });
-//   const page = await browser.newPage();
-
-//   // Replace with the URL of your HTML page
-//   const pageUrl = "https://elitefinancialservices.net/contact.html";
-
-//   try {
-//     // await page.goto(pageUrl, { waitUntil: "domcontentloaded" });
-//     await page.goto(pageUrl, { waitUntil: "domcontentloaded", timeout: 50000 }); // 60 seconds timeout
-
-//     // Fill out the form fields
-//     await page.waitForSelector("#first_name");
-//     await page.type("#first_name", first_name);
-
-//     // // await page.type("#first_name", "John");
-//     await page.waitForSelector("#email");
-//     await page.type("#email", email);
-//     // await page.waitForSelector("#dynamic");
-
-//     await page.waitForSelector("#phone");
-
-//     await page.type("#phone", phone);
-//     await page.waitForSelector("#zip_code");
-
-//     await page.type("#zip_code", zip_code);
-//     await page.waitForSelector("#otherDebt");
-
-//     if (types_of_dept === "Other") {
-//       await page.type("#otherDebt", other_dept_type);
-//     } else {
-//       await page.type("#otherDebt", types_of_dept);
-//     }
-//     await page.waitForFunction(
-//       () => document.querySelectorAll("#menu_2 option").length > 0
-//     );
-//     await page.select("#menu_2", dept_owned);
-
-//     // Click the checkbox
-//     await page.click("#PublisherCheckBox");
-
-//     // Submit the form
-//     await page.click("#submit");
-//     // await page.waitForSelector("#subissionElement", { visible: true });
-
-//     // Wait for a while (adjust this as needed)
-//     await page.waitForTimeout(5000);
-
-//     console.log("Form submitted successfully.");
-//   } catch (error) {
-//     console.error("Error:", error);
-//   } finally {
-//     setTimeout(async () => {
-//       await browser.close();
-//       console.log("Browser closed after 10 seconds.");
-//     }, 40000);
-//   }
-//   res.send("Form submitted successfully.");
-// });
-
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
-
 const express = require("express");
 const puppeteer = require("puppeteer");
 const cors = require("cors");
@@ -100,12 +10,22 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   console.log("first", req.body);
-  res.status(200).json("Pupeeter 2");
+  try {
+    res.status(200).json("Pupee");
+
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
 app.post("/financial-services/token", async (req, res) => {
   console.log(req);
-  res.status(200).json("Send");
+  try {
+    res.status(200).json("Send");
+
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
 app.post("/financial-services", async (req, res) => {
@@ -167,7 +87,11 @@ app.post("/financial-services", async (req, res) => {
     res.status(200).send("Form submitted successfully.");
   } catch (error) {
     console.error("Error:", error);
-    res.status(500).send("Error occurred while submitting the form.",error.message);
+    if (error instanceof puppeteer.errors.TimeoutError) {
+      res.status(500).send(error.message);
+    } else {
+      res.status(500).send(error.message);
+    }
   }
 });
 
@@ -235,7 +159,11 @@ app.post("/accidental-claim", async (req, res) => {
     res.status(200).send("Form submitted successfully.");
   } catch (error) {
     console.error("Error:", error);
-    res.status(500).send("Error occurred while submitting the form.");
+    if (error instanceof puppeteer.errors.TimeoutError) {
+      res.status(500).send(error.message);
+    } else {
+      res.status(500).send(error.message);
+    }
   }
 });
 
@@ -289,8 +217,11 @@ app.post("/medicare", async (req, res) => {
     await browser.close();
     res.status(200).send("Form submitted successfully.");
   } catch (error) {
-    console.error("Error:", error);
-    res.status(500).send("Error occurred while submitting the form.");
+    if (error instanceof puppeteer.errors.TimeoutError) {
+      res.status(500).send(error.message);
+    } else {
+      res.status(500).send(error.message);
+    }
   }
 });
 
@@ -334,9 +265,11 @@ app.post("/being-insured", async (req, res) => {
     await browser.close();
     res.status(200).send("Form submitted successfully.");
   } catch (error) {
-    console.error("Error:", error);
-    throw error(error);
-    res.status(500).send("Error occurred while submitting the form.", error);
+    if (error instanceof puppeteer.errors.TimeoutError) {
+      res.status(500).send(error.message);
+    } else {
+      res.status(500).send(error.message);
+    }
   }
 });
 
